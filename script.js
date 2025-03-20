ce032b3c8d0c557066e4a5ea55e4d93c2a150465
Jonathan Gustavo <jonathan.gustavo.lira@gmail.com>
2025-03-20 12:55:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Obrigado por visitar meu portfólio!');
     });
 });
+
+
+
+const heroTitle = document.querySelector('#hero h1');
+const text = "Olá, eu sou Jonathan Gustavo";
+let index = 0;
+
+function typeWriter() {
+    if (index < text.length) {
+        heroTitle.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 100); // Velocidade da digitação
+    }
+}
+
+typeWriter();
